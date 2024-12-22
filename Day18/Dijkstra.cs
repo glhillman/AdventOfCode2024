ad7e9ef92994ee56b7239a5a9318e415da59c2b8
glhillman <glhillman@gmail.com>
2024-12-22 07:55:37 -0500

    Day18

---
+++ Day18/Dijkstra.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    internal class Dijkstra
+    {
+        public static int FindShortestPath(char[,] maze, (int row, int col) start, (int row, int col) end)
+        {
+            int rows = maze.GetLength(0);
+            int cols = maze.GetLength(1);
+
+            // Directions for moving up, down, left, and right
+            var directions = new (int dRow, int dCol)[] {
+            (-1, 0), (1, 0), (0, -1), (0, 1)
+            };
+
+            // Priority queue to hold nodes to explore
+            var priorityQueue = new SortedSet<(int distance, (int row, int col) position)>();
+            priorityQueue.Add((0, start));
+
+            // Distance dictionary to track shortest distance to each cell
+            var distances = new Dictionary<(int, int), int> { [start] = 0 };
+
+            while (priorityQueue.Count > 0)
+            {
+                var (currentDistance, currentPos) = priorityQueue.Min;
+                priorityQueue.Remove(priorityQueue.Min);
+
+                if (currentPos == end)
+                    return currentDistance;
+
+                foreach (var (dRow, dCol) in directions)
+                {
+                    int newRow = currentPos.row + dRow;
+                    int newCol = currentPos.col + dCol;
+
+                    if (newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols)
+                        continue; // Out of bounds
+
+                    if (maze[newRow, newCol] == '#')
+                        continue; // Wall or obstacle
+
+                    var neighbor = (newRow, newCol);
+                    int newDist = currentDistance + 1;
+
+                    if (!distances.ContainsKey(neighbor) || newDist < distances[neighbor])
+                    {
+                        if (distances.ContainsKey(neighbor))
+                            priorityQueue.Remove((distances[neighbor], neighbor));
+
+                        distances[neighbor] = newDist;
+                        priorityQueue.Add((newDist, neighbor));
+                    }
+                }
+            }
+
+            return -1; // No path found
+        }
+    }
+}
---
+++ Day18/Program.cs
@@ -0,0 +1,132 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day18;
+using System;
+using System.Collections.Generic;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+internal class DayClass
+{
+    List<(int x, int y)> _cells = new();
+    public DayClass()
+    {
+        LoadData();
+    }
+
+    public void Part1()
+    {
+        int size = 71;
+        char[,] maze = new char[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                maze[x,y] = '.';
+            }
+        }
+        for (int i = 0; i < 1024; i++)
+        {
+            maze[_cells[i].x, _cells[i].y] = '#';
+        }
+        int dist = Dijkstra.FindShortestPath(maze, (0, 0), (size - 1, size - 1));
+
+        Console.WriteLine("Part1: {0}", dist);
+    }
+
+    public void Part2()
+    {
+        int size = 71;
+        int startSize = 1024;
+        int maxIndex = _cells.Count -1;
+
+        char[,] maze = new char[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                maze[x, y] = '.';
+            }
+        }
+        for (int i = 0; i < startSize; i++)
+        {
+            maze[_cells[i].x, _cells[i].y] = '#';
+        }
+        bool bigStepsFinished = false;
+        int stepSize = 1000;
+        int nSteps = 1;
+        while (!bigStepsFinished)
+        {
+            MarkRange(maze, startSize, stepSize * nSteps, '#');
+            if (Dijkstra.FindShortestPath(maze, (0, 0), (size - 1, size - 1)) < 1)
+            {
+                MarkRange(maze, startSize, stepSize * nSteps, '.');
+                if (stepSize / 10 == 1)
+                {
+                    MarkRange(maze, startSize, stepSize * (nSteps - 1), '#');
+                    bigStepsFinished = true;
+                }
+                else
+                {
+                    MarkRange(maze, startSize, stepSize * (nSteps - 1), '#');
+                    startSize += stepSize * (nSteps - 1);
+                    stepSize /= 10;
+                    nSteps = 1;
+                }
+            }
+            else
+            {
+                nSteps++;
+            }
+        }
+        int dist;
+        do
+        {
+            startSize++;
+            maze[_cells[startSize].x, _cells[startSize].y] = '#';
+            dist = Dijkstra.FindShortestPath(maze, (0, 0), (size - 1, size - 1));
+        } while (dist > 0);
+
+
+        Console.WriteLine("Part2: {0},{1} ({2})", _cells[startSize].x, _cells[startSize].y, startSize );
+    }
+
+    public void MarkRange(char[,] maze, int start, int count, char c)
+    {
+        for (int i = 0; i+start < _cells.Count && i < count; i++)
+        {
+            maze[_cells[i + start].x, _cells[i+start].y] = c;
+        }
+    }
+
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+        if (File.Exists(inputFile))
+        {
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+                int x = int.Parse(parts[0]);
+                int y = int.Parse(parts[1]);
+                _cells.Add((x, y));
+            }
+
+            file.Close();
+        }
+    }
+
+}
